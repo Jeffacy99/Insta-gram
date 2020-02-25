562a646d86a9b86b2ac105314140c979e9494234
Jeffacy99 <mwaniajapheth1999@gmail.com>
2020-02-25 14:26:07 +0300

    comment form

--- instagram/forms.py
+++ instagram/forms.py
@@ -7,4 +7,16 @@ class PhotoUploadModelForm(forms.ModelForm):
 
     class Meta:
         model = Image
-        fields = [ 'title','image','caption']        
+        fields = [ 'title','image','caption']
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['comment']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_path', 'bio']            
